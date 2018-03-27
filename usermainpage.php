3ffba396e5a9f3f03f898a23b0a2787fe87a60a1
harshit2202 <harshitagarwal49@gmail.com>
2018-03-27 20:30:48 +0530

    User Dashboard

---
+++ usermainpage.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	<link rel="stylesheet" type="text/css" href="usermainpage.css">
+	<title>DashBoard</title>
+</head>
+<body>
+	<div class="main-page" >
+		
+		<div class="heading">
+			<span class="mainheading">Online FIR Portal</span>
+			<p style="float: left ; margin-left: 50%; margin-top: 2%;"; > <img style="margin-bottom: 3%;" src="user.png"> <?php echo "<b>Username&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b>"; ?><a href=""><img src="logout.png"> Logout</a></p>
+		</div>
+
+		<div class ="leftt">
+			
+		</div>
+
+		<div class="news-panel">
+
+		</div>	
+
+	</div>
+</body>
+</html>
