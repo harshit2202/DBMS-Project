3296ae5a5c52e84ffa76fe076f0cb5a7cdbf77ee
Dancing-Goose <prathmesh1998@yahoo.co.in>
2018-03-31 20:23:56 +0530

    more designing to lodge FIR page

--- lodgeFIR.php
+++ lodgeFIR.php
@@ -4,6 +4,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="lodgeFIR.css">
 	<title>
 		Lodge Fir
@@ -11,7 +12,6 @@
 	<script type="text/javascript">
 		function togglefunc()
 		{
-			alert("hello");
 			var x = document.getElementById("suspectselect").value;
 			if(x == "yes")
 			{
@@ -27,7 +27,7 @@
 <body>
 	<div class="global">
 		<div class = "top">
-			Online Fir Portal
+			<h1 class="display-4">Online FIR Portal</h1>
 		</div>
 		<div class ="leftt">
 			<a href="NULL" class = "anchor" >
@@ -48,6 +48,8 @@
 		</div>
 		<div class="main-panel">
 			<form class = "lodge" method = "POST" action="<?php echo htmlspecialchars("$_SERVER[PHP_SELF]");?>">
+				<label for = "victim">Name of Victim</label>
+				<input type="text" name="victim">
 				<label for="date">Date of crime</label>
 				<input type="date" name="date" placeholder="None">
 				<label for="place">Place of crime</label>
@@ -63,9 +65,17 @@
 					<option value="yes">Yes</option>
 				</select>
 				<div id="suspect" style="display: none;">
-				<label for = "suspect" >Description of suspects</label>
-				<textarea rows = "3" name = "suspect"></textarea>
+					<label for = "suspect" >Description of suspects</label>
+					<textarea rows = "3" name = "suspect"></textarea>
 				</div>
+				<label for = "knowsuspect">Do you know suspect</label>
+				<select id = "knowsuspect" name = "knowsuspect">
+					<option value = "select">Select</option>
+					<option value = "yes">Yes</option>
+					<option value = "no">No</option>
+				</select>
+				<br>
+				<button action = "submit">Submit FIR</button>
 			</form>
 		</div>
 	</div>
