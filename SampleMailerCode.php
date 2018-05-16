83a65b6d71f58b4b6e7de8d21e16f3723e2d8fcb
Prathmesh Pathwar <prathmesh1998@yahoo.co.in>
2018-05-16 16:54:09 +0530

    Removed Personal Mails

--- SampleMailerCode.php
+++ SampleMailerCode.php
@@ -9,13 +9,13 @@ try
     $mail->isSMTP();                                      
     $mail->Host = 'smtp.gmail.com';  
     $mail->SMTPAuth = true;                               
-    $mail->Username = 'mridulgupta11944@gmail.com';                
-    $mail->Password = 'mridul1809';                           
+    $mail->Username = 'Enter Sender Mail Here';                
+    $mail->Password = 'Enter Sender Password here';                           
     $mail->SMTPSecure = 'tls';                            
     $mail->Port = 587;                                    
-    $mail->setFrom('mridulgupta11944@gmail.com', 'Mridul');
-    $mail->addAddress('IIT2016028@iiita.ac.in', 'Prathmesh');    
-    $mail->addAddress('IIT2016055@iiita.ac.in', 'Harshit');
+    $mail->setFrom('Enter Sender Mail Here', 'Mridul');
+    $mail->addAddress('Receiver 1', 'Name 1');    
+    $mail->addAddress('Receiver 2', 'Name 2');
     //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
     //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
     $mail->isHTML(true);                                  // Set email format to HTML
